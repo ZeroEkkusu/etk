2e07cb763bf088c46c48e1e30c9f70d3a68c45f8
Zero Ekkusu <94782988+ZeroEkkusu@users.noreply.github.com>
2023-01-27 19:05:31 +0100

    s

--- etk-asm/src/bin/eas.rs
+++ etk-asm/src/bin/eas.rs
@@ -5,7 +5,7 @@ use etk_asm::ingest::{Error, Ingest};
 
 use std::fs::{create_dir_all, File};
 use std::io::prelude::*;
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 
 use clap::StructOpt;
 
@@ -41,19 +41,69 @@ fn main() {
 }
 
 fn run() -> Result<(), Error> {
+    // Parse the command line arguments.
     let opt: Opt = clap::Parser::parse();
 
+    // Check if the output file already exists.
+    let mut out_file_exists = false;
+    let mut out_file_content = vec![];
+    let mut non_existing_directories = vec![];
+    if let Some(o) = &opt.out {
+        if Path::new(o).exists() {
+            out_file_exists = true;
+            match std::fs::read(o) {
+                Ok(content) => out_file_content = content,
+                Err(e) => panic!("couldn't backup existing file: {}", e),
+            }            
+        } else {
+            let mut path = Path::new(o);
+            while !path.exists() {
+                non_existing_directories.push(path);
+                path = path.parent().unwrap();
+            }
+        }
+    }
+
+    // Create an output file handle to write the data to. If the user
+    // did not specify an output file, use standard output.
     let mut out: Box<dyn Write> = match opt.out {
         Some(o) => Box::new(create(o)),
         None => Box::new(std::io::stdout()),
     };
 
+    // Create a wrapper around the output file handle that will write
+    // hexadecimal data to it.
     let hex_out = HexWrite::new(&mut out);
 
+    // Create an Ingest object that will read the data from the input
+    // file and write it to the output file.
     let mut ingest = Ingest::new(hex_out);
-    ingest.ingest_file(opt.input)?;
 
+    // Read the data from the input file and write it to the output file.
+    if let Err(e) = ingest.ingest_file(opt.input) {
+        if out_file_exists {
+            match std::fs::write(&opt.out.unwrap(), &out_file_content) {
+                Ok(_) => (),
+                Err(e) => panic!("couldn't restore existing file: {}", e),
+            };
+        } else if let Some(o) = &opt.out {
+            match std::fs::remove_file(o) {
+                Ok(_) => (),
+                Err(e) => panic!("couldn't remove artifacts: {}", e),
+            }            
+            for non_existing_dir in non_existing_directories {
+                match std::fs::remove_dir(non_existing_dir) {
+                    Ok(()) => (),
+                    Err(e) => panic!("couldn't remove artifacts: {}", e),
+                }                
+            }
+        }
+        return Err(e);
+    }
+
+    // Write a newline to the output file.
     out.write_all(b"\n").unwrap();
 
+    // Exit the program successfully.
     Ok(())
 }
