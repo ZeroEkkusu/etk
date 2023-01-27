56a8da7960018205cedabe643ba536107b0c11da
Zero Ekkusu <94782988+ZeroEkkusu@users.noreply.github.com>
2023-01-27 14:57:45 +0100

    fix: eas

--- etk-asm/src/bin/eas.rs
+++ etk-asm/src/bin/eas.rs
@@ -3,7 +3,7 @@ use etk_cli::io::HexWrite;
 
 use etk_asm::ingest::{Error, Ingest};
 
-use std::fs::File;
+use std::fs::{create_dir_all, File};
 use std::io::prelude::*;
 use std::path::PathBuf;
 
@@ -19,12 +19,32 @@ struct Opt {
 }
 
 fn create(path: PathBuf) -> File {
+    match create_dir_all(path.parent().unwrap()) {
+        Ok(_) => (),
+        Err(why) => panic!("couldn't create parent directories: {}", why),
+    }
+
     match File::create(&path) {
         Err(why) => panic!("couldn't create `{}`: {}", path.display(), why),
         Ok(file) => file,
     }
 }
 
+
+/*
+// This function creates a file at the given path, and returns the file
+// object. If the file cannot be created, the function panics.
+fn create(path: PathBuf) -> File {
+    match File::create(&path) {
+        Err(why) => panic!("couldn't create `{}`: {}", path.display(), why),
+        Ok(file) => file,
+    }
+}
+*/
+
+// This is the main function of the program. It calls run() and
+// prints the error message returned by run() if run() returns
+// an error. Otherwise, it exits the program.
 fn main() {
     let err = match run() {
         Ok(_) => return,
@@ -36,19 +56,59 @@ fn main() {
 }
 
 fn run() -> Result<(), Error> {
+    // Parse the command line arguments.
     let opt: Opt = clap::Parser::parse();
 
+    // Create an Ingest object that will read the data from the input
+    // file and write it to the output file.
+    let mut ingest = Ingest::new(HexWrite::new(&mut vec![]));
+
+    // Read the data from the input file and write it to the vec
+    let res = ingest.ingest_file(opt.input)?;
+
+    // Create an output file handle to write the data to. If the user
+    // did not specify an output file, use standard output.
     let mut out: Box<dyn Write> = match opt.out {
         Some(o) => Box::new(create(o)),
         None => Box::new(std::io::stdout()),
     };
 
-    let hex_out = HexWrite::new(&mut out);
+    // Write the data to the output file
+    out.write_all(&res)?;
+    // Write a newline to the output file.
+    out.write_all(b"\n")?;
 
-    let mut ingest = Ingest::new(hex_out);
-    ingest.ingest_file(opt.input)?;
-
-    out.write_all(b"\n").unwrap();
-
+    // Exit the program successfully.
     Ok(())
 }
+
+/*
+fn run() -> Result<(), Error> {
+    // Parse the command line arguments.
+    let opt: Opt = clap::Parser::parse();
+
+    // Create an output file handle to write the data to. If the user
+    // did not specify an output file, use standard output.
+    let mut out: Box<dyn Write> = match opt.out {
+        Some(o) => Box::new(create(o)),
+        None => Box::new(std::io::stdout()),
+    };
+
+    // Create a wrapper around the output file handle that will write
+    // hexadecimal data to it.
+    let hex_out = HexWrite::new(&mut out);
+
+    // Create an Ingest object that will read the data from the input
+    // file and write it to the output file.
+    let mut ingest = Ingest::new(hex_out);
+
+    // Read the data from the input file and write it to the output file.
+    ingest.ingest_file(opt.input)?;
+
+    // Write a newline to the output file.
+    out.write_all(b"\n").unwrap();
+
+    // Exit the program successfully.
+    Ok(())
+}
+*/
