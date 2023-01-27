28d91f93176e8fb09d3ef74c944dc2ea57062ea7
Zero Ekkusu <94782988+ZeroEkkusu@users.noreply.github.com>
2023-01-27 18:41:24 +0100

    t

--- etk-asm/src/bin/eas.rs
+++ etk-asm/src/bin/eas.rs
@@ -3,7 +3,7 @@ use etk_cli::io::HexWrite;
 
 use etk_asm::ingest::{Error, Ingest};
 
-use std::fs::{create_dir_all, File};
+use std::fs::File;
 use std::io::prelude::*;
 use std::path::PathBuf;
 
@@ -30,75 +30,30 @@ fn create(path: PathBuf) -> File {
     }
 }
 
+fn main() {
+    let err = match run() {
+        Ok(_) => return,
+        Err(e) => e,
+    };
 
-/*
-// This function creates a file at the given path, and returns the file
-// object. If the file cannot be created, the function panics.
-fn create(path: PathBuf) -> File {
-    match File::create(&path) {
-        Err(why) => panic!("couldn't create `{}`: {}", path.display(), why),
-        Ok(file) => file,
-    }
+    eprintln!("{}", WithSources(err));
+    std::process::exit(1);
 }
-*/
 
-// This is the main function of the program. It calls run() and
-// prints the error message returned by run() if run() returns
-// an error. Otherwise, it exits the program.
 fn run() -> Result<(), Error> {
-    // Parse the command line arguments.
     let opt: Opt = clap::Parser::parse();
 
-    // Check if the output file already exists.
-    let mut out_file_exists = false;
-    let mut out_file_content = vec![];
-    let mut non_existing_directories = vec![];
-    if let Some(o) = &opt.out {
-        if Path::new(o).exists() {
-            out_file_exists = true;
-            out_file_content = std::fs::read(o)?;
-        } else {
-            let mut path = Path::new(o);
-            while !path.exists() {
-                non_existing_directories.push(path);
-                path = path.parent().unwrap();
-            }
-        }
-    }
-
-    // Create an output file handle to write the data to. If the user
-    // did not specify an output file, use standard output.
     let mut out: Box<dyn Write> = match opt.out {
-        Some(o) => Box::new(create(o)?),
+        Some(o) => Box::new(create(o)),
         None => Box::new(std::io::stdout()),
     };
 
-    // Create a wrapper around the output file handle that will write
-    // hexadecimal data to it.
     let hex_out = HexWrite::new(&mut out);
 
-    // Create an Ingest object that will read the data from the input
-    // file and write it to the output file.
     let mut ingest = Ingest::new(hex_out);
+    ingest.ingest_file(opt.input)?;
 
-    // Read the data from the input file and write it to the output file.
-    if let Err(e) = ingest.ingest_file(opt.input) {
-        if out_file_exists {
-            std::fs::write(&opt.out.unwrap(), &out_file_content)?;
-        } else if let Some(o) = &opt.out {
-            std::fs::remove_file(o)?;
-            for non_existing_dir in non_existing_directories {
-                std::fs::remove_dir(non_existing_dir)?;
-            }
-        }
-        return Err(e);
-    }
-
-    // Write a newline to the output file.
     out.write_all(b"\n").unwrap();
 
-    // Exit the program successfully.
     Ok(())
 }
-
-
